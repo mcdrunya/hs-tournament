0ab268e6589094577a3232a1c9cfa811ec38edea
mcdrunya <gand2ry@gmail.com>
2019-12-15 06:58:51 +0300

    first commit

---
+++ js/scriptsmooth.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    $("#scroll").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+ 
+
+        //забираем идентификатор блока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+ 
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+
+         
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 2500);
+
+    });
+
+});
+
+
+$(document).ready(function(){
+
+    $("#scroll2").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+ 
+
+        //забираем идентификатор бока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+ 
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+
+         
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 2500);
+
+    });
+
+});
+
